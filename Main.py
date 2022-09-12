eb123a3a1bb86024e9575ac175d0e17710fff184
BISWAYAN MEHRA <108832092+Biswayan-Mehra@users.noreply.github.com>
2022-09-12 15:36:35 +0530

    Biswayan Mehra 1.1

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ def insertionSort(array) -> List[int]:
     for j in range(i+1,0,-1):
       if array[min_index]>array[j]:
         array[min_index], array[j] = array[j], array[min_index] 
-        min_index = j--
+        min_index = j-1
 
 # data = [9, 5, 1, 4, 3]
 input_data = input()
